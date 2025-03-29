80826245e071ceab25a9fb5b28625ddeca28872d
Anshraj <152844802+Anshraj06@users.noreply.github.com>
2025-03-29 13:24:45 +0000

    container config done

---
+++ javas/Dataype.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3)
+
+console.log(3+3)
+console.log("Ansh raj ")
+
+
+let name ="Ansh raj"
+let age=18
+let isLoggedIN =false
+
+// Type of Data type
+// 1>Number [2 to power of53]
+// 2>string=>""
+// Boolean=>True or False
+// Null = Standalone Value
+// undefined =>
+// Symbol =>Unique
+
+// Object
+
+console.log(typeof age)
+
